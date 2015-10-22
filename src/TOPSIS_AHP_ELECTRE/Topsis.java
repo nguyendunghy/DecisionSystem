7fd4d07f2dde397f6fbb4b6854a1b8d9254205a5
nguyendunghy <dung.kstncnttk56@gmail.com>
2015-10-22 09:36:25 +0700

    Thêm hàm getResultRow đề trả về thứ tự các lựa chọn theo thứ tự giảm dần Sửa lại hàm getResult thêm phần sắp xếp lại giá trị các phương án và vị trí tương ứng của chúng

--- src/TOPSIS_AHP_ELECTRE/Topsis.java
+++ src/TOPSIS_AHP_ELECTRE/Topsis.java
@@ -11,14 +11,15 @@ import java.util.Arrays;
  *
  * @author NguyenVanDung
  * @version 1 Đầu vào là một mảng hai chiều bảng đữ liệu đã nhân trong số Đầu ra
- * trả về dòng có giá trị sau khi xử lí tốt nhất
+ * trả về dòng có giá trị sau khi xử lí tốt nhất và thứ tự các đáp án theo thứ
+ * tự giảm dần
  *
  */
 public class Topsis {
 
     private int row;   //số dòng của bảng
     private int column;// số cột của bản
-    private int chosenRow;//Hàng có giá trị tốt nhất sau khi xử lí
+    private int[] resultRow;//Hàng có giá trị tốt nhất sau khi xử lí
     private float[] table;//bảng dữ liệu
     private float[] Astar;//Mảng chứa giá trị giá trị tốt nhất
     private float[] Aminus;//Mảng chứa giá trị tồi nhất
@@ -40,7 +41,12 @@ public class Topsis {
     public static void main(String[] args) {
         float[] input = new float[]{(float) 0.1314, (float) 0.0711, (float) 0.0925, (float) 0.0564, (float) 0.0346, (float) 0.1124, (float) 0.0894, (float) 0.0719, (float) 0.0484, (float) 0.0865, (float) 0.1143, (float) 0.0826, (float) 0.0822, (float) 0.0645, (float) 0.0605, (float) 0.1181, (float) 0.0871, (float) 0.0719, (float) 0.0806, (float) 0.0519, (float) 0.1333, (float) 0.0642, (float) 0.1027, (float) 0.0323, (float) 0.0519, (float) 0.1238, (float) 0.0917, (float) 0.0616, (float) 0.0726, (float) 0.0692};
         Topsis top = new Topsis(6, 5, input);
-        System.out.println(String.valueOf(top.getChosenRow()));
+        // System.out.println(String.valueOf(top.getChosenRow()));
+
+        int[] rs = top.getResultRow();
+        for (int i = 0; i < top.row; i++) {
+            System.out.print(rs[i] + " ");
+        }
     }
 
     //Tính các khoảng cách từ các dòng đến các giá trị lí tưởng
@@ -100,27 +106,55 @@ public class Topsis {
     //Đây chỉ là một hàm,mình có thể mở rộng ra ra nhiều hàm khác
     private void GetResult() {
         float C[] = new float[row];
-        int maxRow;  //Dòng có giá trị tốt nhất
+        float orderedValueOfC[] = new float[row];
         //Tính vector C
         for (int i = 0; i < row; i++) {
             C[i] = Sminus[i] / (Sminus[i] + Sstar[i]);
         }
+        //Sao chép giá trị của C sang orderedValueOfC
+        orderedValueOfC = Arrays.copyOfRange(C, 0, row);
 
-        maxRow = 0;
+        //Sắp xếp các giá trị của các lựa chọn theo thứ tự giảm dần
+        //Mảng resultRow lúc đầu chứa giá trị bằng chính thứ tự của các phần tử
+        resultRow = new int[row];
         for (int i = 0; i < row; i++) {
-            if (C[i] > C[maxRow]) {
-                maxRow = i;
-            }
+            resultRow[i] = i;
+
         }
 
-        chosenRow = maxRow;
+        for (int i = 0; i < row; i++) {
+            int max = i;
+            for (int j = i + 1; j < row; j++) {
+                if (orderedValueOfC[j] > orderedValueOfC[max]) {
+                    max = j;
+                }
+            }
+            //Đổi giá trị của orderedValueOfC[i] và orderedValueOfC[max]
+            float temp = orderedValueOfC[max];
+            orderedValueOfC[max] = orderedValueOfC[i];
+            orderedValueOfC[i] = temp;
+
+            //Lấy vị trí của lựa chọn tốt tương ứng
+            int tt = resultRow[max];
+            resultRow[max] = resultRow[i];
+            resultRow[i] = tt;
+
+        }
+
     }
 
     //Hàm trả về dòng có giá trị tốt nhất
     public int getChosenRow() {
         calculateDistance();
         GetResult();
-        return chosenRow;
+        return resultRow[0];
+    }
+
+    //Hàm trả về thứ tự các lựa chọn
+    public int[] getResultRow() {
+        calculateDistance();
+        GetResult();
+        return resultRow;
     }
 
 }
